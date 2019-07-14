a11f98d7240859f077e7ba30f2e5c21b5f2dfaf6
Santos Jiménez Linares <sjimenez77@gmail.com>
2019-07-14 19:24:36 +0200

    Play button added

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { Header } from '../../components';
 import styles from './styles';
 
 export default class Home extends Component {
+  onPlayPress = () => {
+    console.log('onPlayPress event handler');
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Header />
-        {/* // this View is empty on purpose! */}
+        <TouchableOpacity
+          onPress={this.onPlayPress}
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Image
+            source={require('../../assets/icons/play_arrow.png')}
+            style={styles.playIcon}
+          />
+          <Text style={styles.play}>PLAY!</Text>
+        </TouchableOpacity>
       </View>
     );
   }
--- screens/Home/styles.tsx
+++ screens/Home/styles.tsx
@@ -7,4 +7,15 @@ export default StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  play: {
+    fontSize: 45,
+    fontFamily: 'dogbyte',
+    color: '#ecf0f1',
+    marginTop: 5,
+  },
+  playIcon: {
+    height: 60,
+    width: 60,
+    marginRight: 15,
+  },
 });
